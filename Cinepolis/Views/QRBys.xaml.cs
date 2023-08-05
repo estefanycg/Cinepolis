706cbc6b70b4f21ee3e473c4edb624afaddbd5d0
ElsyAmaya <pao.amayalazo01@gmail.com>
2023-08-05 00:31:23 -0600

    Agg Views QRBys

---
+++ Cinepolis/Views/QRBys.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cinepolis.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRBys : ContentPage
+    {
+        public QRBys()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, true);
+        }
+        private async void ButtonFinalizarClicked(object sender, EventArgs e)
+        {
+
+            Application.Current.MainPage = new AppShell();
+
+        }
+    }
+}
